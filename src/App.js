3cf9ec51f5786410d95a1722a0c96b98dd79cbe7
Abduboriy1 <aaauzbek@gmail.com>
2022-06-08 23:13:23 -0500

    deploy my react app to gitHub pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App!</h1>
+      <h2>Author Abduboriy</h2>
+      <img src={logo} alt="profile-image"></img>
+    </div>
+  );
+}
+
+export default App;
